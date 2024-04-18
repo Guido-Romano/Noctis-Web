2791cf4da77265227c3864efa0efaf4c3072d531
apocca <guidoromano1989@gmail.com>
2024-04-18 16:23:25 -0300

    Actualización de los archivos de producción

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
 import { Team1Component } from './teams/team1/team1.component'; 
-
-
-import { ContactComponent } from './contact/contact.component';
-import { Creator1Component } from './creators/creator1/creator1.component'; // Importa el componente específico para el Creador N1
-import { Creator2Component } from './creators/creator2/creator2.component'; // Importa el componente específico para el Creador N2
-import { Creator3Component } from './creators/creator3/creator3.component'; // Importa el componente específico para el Creador N3
+import { CreatorsComponent } from './creators/creators.component';
+import { StaffComponent } from './staff/staff.component';
+import { AboutUsComponent } from './about-us/about-us.component'; // Corregido
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'teams', redirectTo: 'teams/team1', pathMatch: 'full' },
   { path: 'teams/team1', component: Team1Component, data: { title: 'Counterstrike' } },
-  { path: 'contact', component: ContactComponent },
-  { path: 'creators/creator1', component: Creator1Component, data: { title: 'Creador1' } }, // Ruta para el Creador N1
-  { path: 'creators/creator2', component: Creator2Component, data: { title: 'Creadordos' } }, // Ruta para el Creador N2
-  { path: 'creators/creator3', component: Creator3Component, data: { title: 'Creadortres' } }, // Ruta para el Creador N3
-  { path: 'creators', redirectTo: 'creators/creator1', pathMatch: 'full' }, // Redirecciona al primer creador por defecto
+  { path: 'creators', component: CreatorsComponent },
+  { path: 'staff', component: StaffComponent },
+  { path: 'about-us', component:  AboutUsComponent},
   { path: '**', redirectTo: '', pathMatch: 'full'} // Ruta por defecto
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,12 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TeamsComponent } from './teams/teams.component';
 import { CreatorsComponent } from './creators/creators.component';
-import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing.module';
 import { Team1Component } from './teams/team1/team1.component';
-import { Creator1Component } from './creators/creator1/creator1.component';
-import { Creator2Component } from './creators/creator2/creator2.component';
-import { Creator3Component } from './creators/creator3/creator3.component';
-import { Imagen1Component } from './home/imagen1/imagen1.component';
-import { Imagen2Component } from './home/imagen2/imagen2.component';
-import { Imagen3Component } from './home/imagen3/imagen3.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { StaffComponent } from './staff/staff.component';
+import { LogoComponent } from './teams/team1/logo/logo.component';
+
 
 @NgModule({
   declarations: [
@@ -21,14 +18,10 @@ import { Imagen3Component } from './home/imagen3/imagen3.component';
     HomeComponent,
     TeamsComponent,
     CreatorsComponent,
-    ContactComponent,
     Team1Component,
-    Creator1Component,
-    Creator2Component,
-    Creator3Component,
-    Imagen1Component,
-    Imagen2Component,
-    Imagen3Component
+    AboutUsComponent,
+    StaffComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
